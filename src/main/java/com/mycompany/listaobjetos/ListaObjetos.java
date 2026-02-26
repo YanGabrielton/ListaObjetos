b64254ab03b62fa01a0492e87718f7700720969a
AsRok <yangabrielton@gmail.com>
2026-02-26 11:16:16 -0300

    update

--- src/main/java/com/mycompany/listaobjetos/ListaObjetos.java
+++ src/main/java/com/mycompany/listaobjetos/ListaObjetos.java
@@ -5,6 +5,8 @@
 package com.mycompany.listaobjetos;
 
 import com.mycompany.listaobjetos.model.Usuario;
+import com.mycompany.listaobjetos.model.UsuarioPasta;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,28 +18,32 @@ import java.util.UUID;
  */
 public class ListaObjetos {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        
         
         Scanner input= new Scanner(System.in);
         List<Usuario>usuarios = new ArrayList<>();
         
         UUID token=UUID.randomUUID();
-        System.out.println("Digite nome;");
+        System.out.println("Digite nome: ");
         String nome= input.next();
         
-        System.out.println("Digite email;");
+        System.out.println("Digite email: ");
         String email= input.next();
         
-        System.out.println("Digite idade;");
+        System.out.println("Digite idade: ");
         int idade= input.nextInt();
         
        Usuario u=new Usuario(token,nome,email,idade);
        usuarios.add(u);
-        
+          UsuarioPasta pasta = new UsuarioPasta();
+    pasta.SalvarUsuarios(u);
        for(Usuario user :usuarios){
        
-       
+           System.out.println("Token"+user.getIdUser()+
+                   "\n Nome: "+user.getNome()+
+                   "\n Email: "+user.getEmail()+
+                   "\n idade: "+user.getIdade());
        
        }
         
